af6a590b78a3d56c58cb0d659f936ef66909af70
SJX1994 <752523247@qq.com>
2022-09-05 09:15:04 +0800

    f my home

---
+++ b_笔记/Lang/Cmake/example/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "adder.h"
+// float add(float x, float y); //同一目录下
+
+int main()
+{
+    std::cout << "Hello World SJX \n";
+
+    std::cout << add(123.0f, 1.0f) << '\n';
+    return 0;
+}
